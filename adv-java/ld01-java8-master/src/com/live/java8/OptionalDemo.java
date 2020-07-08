4e173a0313ab62682616a199ba12eef70832a5fe
live-git-subbus <crafted.coded.2003@gmail.com>
2020-07-08 06:50:53 +0530

    .

--- adv-java/ld01-java8-master/src/com/live/java8/OptionalDemo.java
+++ adv-java/ld01-java8-master/src/com/live/java8/OptionalDemo.java
@@ -3,10 +3,21 @@ package com.live.java8;
 import java.util.ArrayList;
 
 public class OptionalDemo {
+	static String testString;
+
 	public static void main(String[] args) {
 		Person person = null;
 		ArrayList arrayList = null;
 		Integer numbers[] = null;
+
+		if (Math.random() > 0.5) {
+			numbers = new Integer[3];
+			numbers[0] = 100;
+			testString = "Welcome";
+			person = new Person();
+			arrayList = new ArrayList();
+			arrayList.add("smile");
+		}
 	}
 }
 
