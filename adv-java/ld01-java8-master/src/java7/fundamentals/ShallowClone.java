c7a149495e4b48a106077ee846efc89f7644f6f5
live-git-subbus <crafted.coded.2003@gmail.com>
2020-07-11 07:25:00 +0530

    .

--- adv-java/ld01-java8-master/src/java7/fundamentals/ShallowClone.java
+++ adv-java/ld01-java8-master/src/java7/fundamentals/ShallowClone.java
@@ -1,12 +1,14 @@
 package java7.fundamentals;
 
+import java.util.Arrays;
+
 public class ShallowClone {
 public static void main(String[] args) {
-
+ObjectToBeCloned.cloneTest();
 }
 }
 
-class ObjectToBeCloned {
+class ObjectToBeCloned implements Cloneable{
 	// THE INT ARRAY IS SHALLOW CLONED AS ONLY THE ADDRESS IS COPIED AND THE VALUES
 	// ARE NOT COPIED
 	public int[] numbers;
@@ -22,6 +24,33 @@ class ObjectToBeCloned {
 	static void cloneTest() {
 		ObjectToBeCloned originalObject = new ObjectToBeCloned(2);
 		originalObject.numbers[0] = 100;
+		
+		try {
+			ObjectToBeCloned clonedObject = (ObjectToBeCloned)originalObject.clone();
+			System.out.println("Original      " + originalObject);
+			System.out.println("Cloned      " + clonedObject);
+			
+			clonedObject.nameString = "John";
+			clonedObject.floatWrapper = 789.987f;
+			clonedObject.test.nameString = "Virus!";
+			clonedObject.value = 500.500f;
+			//originalObject.value = 500.500f;
+			clonedObject.numbers[1] = 5000;
+			
+//			// AFTER CLONING
+			System.out.println("Original      " + originalObject);
+			System.out.println("Cloned      " + clonedObject);
+			
+		} catch (CloneNotSupportedException e) {
+			System.out.println("You cannot clone this object!" + e);
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "ObjectToBeCloned [numbers=" + Arrays.toString(numbers) + ", nameString=" + nameString + ", value="
+				+ value + ", floatWrapper=" + floatWrapper + ", test=" + test + "]";
 	}
 }
 class Test {
