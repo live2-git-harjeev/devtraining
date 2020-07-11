e96577ca9c482365ba9c89d4b9cbc51481e46833
live-git-subbus <crafted.coded.2003@gmail.com>
2020-07-11 20:26:54 +0530

    .

---
+++ adv-java/ld01-java8-master/src/jdbc/crud/util/DatabaseType.java
@@ -0,0 +1,5 @@
+package jdbc.crud.util;
+
+public enum DatabaseType {
+ORACLEDB, MYSQLDB
+}
---
+++ adv-java/ld01-java8-master/src/jdbc/crud/util/TestConnections.java
@@ -0,0 +1,8 @@
+package jdbc.crud.util;
+
+public class TestConnections {
+public static void main(String[] args) {
+	DatabaseUtil.testOracleConnection();
+	DatabaseUtil.testMySQLConnection();
+}
+}
