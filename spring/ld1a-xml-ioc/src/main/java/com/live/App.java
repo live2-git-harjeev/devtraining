6b49fed2d1dac741dd881682b9fc23f5513bb598
live-git-subbus <crafted.coded.2003@gmail.com>
2020-07-15 06:26:26 +0530

    .

--- spring/ld1a-xml-ioc/src/main/java/com/live/App.java
+++ spring/ld1a-xml-ioc/src/main/java/com/live/App.java
@@ -1,14 +1,26 @@
 package com.live;
 
-import com.live.service.ProductService;
-import com.live.service.ProductServiceImpl;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.live.repository.ProductRepository;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Enterprise Application starter!" );
-        ProductService productService = new ProductServiceImpl();
-        productService.getProducts().forEach(System.out::println);
+//        System.out.println( "Enterprise Application starter!" );
+//        ProductService productService = new ProductServiceImpl();
+//        productService.getProducts().forEach(System.out::println);
+    	
+//    	ProductRepository productRepository = new OracleRepository();
+    	ProductRepository productRepository = null;
+    	
+//    	Load the spring container
+//    	The spring container would use the applicationContext.xml to configure the spring beans
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    	
+    	//Access the spring bean from the application
+    	productRepository = (ProductRepository)context.getBean("productRepository");
+    	productRepository.getProducts().forEach(System.out::println);
     }
 }
