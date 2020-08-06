d44a68efb36edd9bb07c66e0f9ee096365d1c660
live-git-subbus <crafted.coded.2003@gmail.com>
2020-08-06 07:19:03 +0530

    .

---
+++ docs/g security.md
@@ -0,0 +1,10 @@
+:closed_book: **Security**
+
+:label: ***Authentication & Authorization***
+- Who is the user? Authentication
+- What  action is the user willing to perform? Authorization
+
+- Spring has a secuirty module
+- Principal object is used by spring to identify the logged in user.
+
+
---
+++ spring-boot/ld6a-sb-security-authentication/src/main/java/com/live/Ld6aSbSecurityAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ld6aSbSecurityAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ld6aSbSecurityAuthenticationApplication.class, args);
+	}
+}
